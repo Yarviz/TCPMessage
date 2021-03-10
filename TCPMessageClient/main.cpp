eb89961fe493cede67328f662a3f4f25a6cad0e3
Yarviz <juzzzzi@hotmail.com>
2021-03-10 12:18:19 +0200

    update

---
+++ TCPMessageClient/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+
+#include "client.h"
+
+int main(int argc, const char **argv)
+{
+    //if (argc > 1 && strcmp(argv[1], "-server") == 0)
+    std::cout << "Start Client!" << std::endl;
+    Client client;
+
+    return 0;
+}
---
+++ TCPMessageServer/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+
+#include "server.h"
+
+int main(int argc, const char **argv)
+{
+    //if (argc > 1 && strcmp(argv[1], "-server") == 0)
+    std::cout << "Start Server!" << std::endl;
+    Server server;
+
+    return 0;
+}
